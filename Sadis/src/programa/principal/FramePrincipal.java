7c84ae33f50b7ed6447dfb8f7c7a5f019139f84b
Thiago de Oliveira Santos <thiagosantos@dcc.ufba.br>
2015-06-09 15:59:47 -0300

    limpando imports desnecessarios do frame principal

--- Sadis/src/programa/principal/FramePrincipal.java
+++ Sadis/src/programa/principal/FramePrincipal.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
