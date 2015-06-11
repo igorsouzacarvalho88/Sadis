5e6347d06c2b53c5ade1b2d6effc52243049c5ef
Igor Souza Carvalho <igorsouzac@dcc.ufba.br>
2015-06-11 17:48:07 -0300

    Conexão com o banco de dados realizada com sucesso.

---
+++ Sadis/src/test/Connect.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.sql.*;
+
+public class Connect
+{
+    public static void main (String[] args ){
+        
+    	Connection conn = null;
+
+        try
+        {
+            String userName = "root";
+            
+            String password = "";
+            
+            String url = "jdbc:mysql://localhost/Sadis";
+            
+            Class.forName ("com.mysql.jdbc.Driver");  
+            
+            conn = DriverManager.getConnection (url, userName, password);
+            
+            System.out.println ("Conexão com o BD estabelecida!");
+        }
+        catch (Exception e)
+        {
+            System.err.println ("Não foi possível estabelecer conexão com o BD");
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close ();
+                    System.out.println ("Conexão finalizada");
+                }
+                catch (Exception e) { /* ignore close errors */ }
+            }
+        }
+    }
+}
