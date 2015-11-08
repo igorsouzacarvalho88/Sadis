550dfc06e1094bbbd4a0422c4d4215fa77928d32
Rafael Coimbra <rafael@rafaelcoimbra.com.br>
2015-11-08 12:38:55 -0300

    Limpando a interface

--- Sadis/src/programa/principal/FrameSolicitacao.java
+++ Sadis/src/programa/principal/FrameSolicitacao.java
@@ -29,6 +29,16 @@ import javax.swing.JCheckBoxMenuItem;
 import java.awt.Label;
 import java.awt.Color;
 import java.awt.TextArea;
+import java.awt.Button;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
+import javax.swing.border.LineBorder;
+import javax.swing.JDesktopPane;
+import java.awt.Font;
+import javax.swing.JProgressBar;
+import javax.swing.JTextPane;
 
 public class FrameSolicitacao extends JFrame {
 
@@ -62,11 +72,11 @@ public class FrameSolicitacao extends JFrame {
 		setContentPane(contentPane);
 		JButton btnEnviar = new JButton("Enviar");
 		nome = new JTextField();
-		nome.setBounds(155, 57, 253, 20);
+		nome.setBounds(155, 50, 253, 25);
 		nome.setColumns(10);
 
 		matricula = new JTextField();
-		matricula.setBounds(155, 88, 253, 20);
+		matricula.setBounds(155, 80, 253, 25);
 		matricula.setColumns(10);
 
 		contentPane.setLayout(null);
@@ -76,30 +86,30 @@ public class FrameSolicitacao extends JFrame {
 
 		telefone = new JTextField();
 		telefone.setColumns(10);
-		telefone.setBounds(155, 119, 253, 20);
+		telefone.setBounds(155, 110, 253, 25);
 		contentPane.add(telefone);
 
 		email = new JTextField();
 		email.setColumns(10);
-		email.setBounds(155, 150, 253, 20);
+		email.setBounds(155, 140, 253, 25);
 		contentPane.add(email);
 
-		btnEnviar.setBounds(60, 532, 87, 23);
+		btnEnviar.setBounds(35, 436, 87, 23);
 
 		lblInsiraSeuNome = new JLabel("Nome Completo");
-		lblInsiraSeuNome.setBounds(25, 60, 99, 14);
+		lblInsiraSeuNome.setBounds(25, 55, 99, 14);
 		contentPane.add(lblInsiraSeuNome);
 
 		lblInsiraSuaMatricula = new JLabel("Matricula");
-		lblInsiraSuaMatricula.setBounds(25, 91, 99, 14);
+		lblInsiraSuaMatricula.setBounds(25, 85, 99, 14);
 		contentPane.add(lblInsiraSuaMatricula);
 
 		lblTelefone = new JLabel("Telefone / Celular");
-		lblTelefone.setBounds(25, 122, 99, 14);
+		lblTelefone.setBounds(25, 115, 99, 14);
 		contentPane.add(lblTelefone);
 
 		lblEmail = new JLabel("Email");
-		lblEmail.setBounds(25, 157, 99, 14);
+		lblEmail.setBounds(25, 145, 99, 14);
 		contentPane.add(lblEmail);
 
 		JButton btnCancelar = new JButton("Cancelar");
@@ -108,7 +118,7 @@ public class FrameSolicitacao extends JFrame {
 				dispose(); // fecha o Jframe
 			}
 		});
-		btnCancelar.setBounds(341, 532, 89, 23);
+		btnCancelar.setBounds(321, 436, 87, 23);
 		contentPane.add(btnCancelar);
 
 		codigoMateria = new JTextField();
@@ -117,10 +127,10 @@ public class FrameSolicitacao extends JFrame {
 				codigoMateria.setText(""); // faz com que o campo de codigos seja limpo sempre que clicar nele
 				}
 			});
-		codigoMateria.setBounds(35, 225, 68, 20);
+		codigoMateria.setBounds(35, 225, 68, 25);
 		contentPane.add(codigoMateria);
 		codigoMateria.setColumns(10);	
-		JLabel lblInsiraOCodigo = new JLabel("Insira o codigo das diciplinas, uma a uma e clique em Ok");
+		JLabel lblInsiraOCodigo = new JLabel("Insira o codigo da diciplina");
 		lblInsiraOCodigo.setBounds(25, 204, 383, 14);
 		contentPane.add(lblInsiraOCodigo);
 
@@ -160,33 +170,19 @@ public class FrameSolicitacao extends JFrame {
 				"Formul\u00E1rio de solicita\u00E7\u00E3o");
 		lblFormulrioDeSolicitao.setBounds(178, 11, 196, 14);
 		contentPane.add(lblFormulrioDeSolicitao);
-
-		lblExMata = new JLabel(
-				"Ex: MATA01 ( Clique em ok ) MATA02 ( Clique em ok )...");
-		lblExMata.setBounds(25, 275, 383, 14);
-		contentPane.add(lblExMata);
-
-		lblAoAdicionarTodas = new JLabel(
-				"Ao adicionar todas as matérias clique em Enviar.");
-		lblAoAdicionarTodas.setBounds(25, 325, 383, 14);
-		contentPane.add(lblAoAdicionarTodas);
 		
 		JLabel lblI = new JLabel("Espaço para justificativa do processo de aproveitamento");
-		lblI.setBounds(25, 369, 383, 14);
+		lblI.setBounds(25, 286, 383, 14);
 		contentPane.add(lblI);
 		
 		final TextArea justificativa = new TextArea("",4,30, TextArea.SCROLLBARS_VERTICAL_ONLY);
-		justificativa.setBounds(35, 394, 373, 98);
+		justificativa.setBounds(35, 311, 373, 98);
 		contentPane.add(justificativa);
 		
 		dicipsol = new JLabel(" ");
 		dicipsol.setForeground(Color.RED);
 		dicipsol.setBounds(25, 249, 383, 20);
 		contentPane.add(dicipsol);
-		
-		JLabel lblConsultaVerificaA = new JLabel("Consulta: verifica o nome da matéria.");
-		lblConsultaVerificaA.setBounds(25, 300, 187, 14);
-		contentPane.add(lblConsultaVerificaA);
 		
 		
 		
@@ -217,17 +213,21 @@ public class FrameSolicitacao extends JFrame {
 		
 		btnEnviar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) { // EVENTO ao clicar no bot�o enviar verifica se os campos do formulario não estão em branco
+				// Validação das informações adicionadas matricula, telefone e email
 				int v = 1;
+				lblEmail.setForeground(Color.BLACK);
 				if ((!email.getText().contains("@")) || (!email.getText().contains("."))){
 					JOptionPane.showMessageDialog(null, "Campo e-mail incorreto");
 					lblEmail.setForeground(Color.RED);
 					v=0;
 				}
+				lblTelefone.setForeground(Color.BLACK);	
 				if ((!telefone.getText().matches("[0-9( )-]*"))){
 					JOptionPane.showMessageDialog(null, "Campo telefone incorreto");
 					lblTelefone.setForeground(Color.RED);	
 					v=0;
 				}
+				lblInsiraSuaMatricula.setForeground(Color.BLACK);
 				if ((!matricula.getText().matches("[0-9]*")) || (matricula.getText().length()>9) || (matricula.getText().length()<9)){
 					JOptionPane.showMessageDialog(null, "Campo matricula incorreto");
 					lblInsiraSuaMatricula.setForeground(Color.RED);	
@@ -280,7 +280,4 @@ public class FrameSolicitacao extends JFrame {
 	//private IAlunoDAO daoAluno = new AlunoDAO();
 	//private ISolicitacaoDAO daoSolicitacao = new SolicitacaoDAO();
 	Aproveitamento aproveitamento = new Aproveitamento();
-
-	private JLabel lblExMata;
-	private JLabel lblAoAdicionarTodas;
 }
